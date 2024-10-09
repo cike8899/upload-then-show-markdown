929d7e85fad1b0c45f5ae5dc21f4e78d08476081
bill <yibie_jingnian@163.com>
2024-10-09 10:58:22 +0800

    feat: Add Spinner to page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,26 @@
 import HightLightMarkdown from "@/components/highlight-markdown";
 import { ChangeEventHandler, useCallback, useState } from "react";
+import { ClipLoader } from "react-spinners";
 
 export default function HomePage() {
   const [text, setText] = useState("");
+  const [loading, setLoading] = useState(false);
   const upload: ChangeEventHandler<any> = useCallback(async (e) => {
-    console.log("🚀 ~ upload ~ params:", e.target.files);
-    const files = e.target.files;
-    const formData = new FormData();
-    formData.append("file", files[0]);
+    try {
+      setLoading(true);
+      console.log("🚀 ~ upload ~ params:", e.target.files);
+      const files = e.target.files;
+      const formData = new FormData();
+      formData.append("file", files[0]);
 
-    const ret = await fetch("/upload", { method: "POST", body: formData });
-    const data = await ret.text();
-    if (data) {
-      setText(data);
+      const ret = await fetch("/upload", { method: "POST", body: formData });
+      const data = await ret.text();
+      if (data) {
+        setText(data);
+      }
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
     }
   }, []);
 
@@ -22,8 +30,12 @@ export default function HomePage() {
       <div>
         <input type="file" name="" id="" onChange={upload} accept=".pdf" />
       </div>
-      <div style={{ height: 800, width: "100%" }}>
-        <HightLightMarkdown>{text}</HightLightMarkdown>
+      <div style={{ height: 800, width: "100%", padding: 20 }}>
+        {loading ? (
+          <ClipLoader color="#2d56ef" size={50}></ClipLoader>
+        ) : (
+          <HightLightMarkdown>{text}</HightLightMarkdown>
+        )}
       </div>
     </div>
   );
